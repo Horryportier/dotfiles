c0b1e3fdc6cad9b6b8d81a6833aa36d7cc905423
HorryPortier <horryportier@pop-os.localdomain>
2023-04-15 15:35:07 +0200

    Sat Apr 15 03:35:07 PM CEST 2023

---
+++ nvim/nvim/lua/HorryPortier/set.lua
@@ -0,0 +1,25 @@
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.exrc = true
+vim.opt.relativenumber = true
+vim.opt.nu = true
+vim.opt.hidden = true
+vim.opt.scrolloff = 8
+vim.opt.syntax = "no"
+vim.opt.encoding = "UTF-8"
+vim.opt.spell = true
+vim.opt.hlsearch = false
+vim.opt.colorcolumn = "100"
+vim.opt.mouse = ""
+
+vim.wo.wrap = false
+
+vim.g.mapleader = " "
+vim.g.UltiSnipsEditSplit="vertical"
+
+vim.api.nvim_command('autocmd BufEnter * EnableBlameLine')
+
+
+
